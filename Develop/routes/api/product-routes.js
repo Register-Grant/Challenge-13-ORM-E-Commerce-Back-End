b591b5b35ab533560c52dcc9e2b18f506dd23ff0
Grant Register <register.grant@gmail.com>
2022-08-18 18:28:10 -0600

    finally got the connection.js file working - stupid starter code

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -32,7 +32,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   try {
-    const productId = await Product.findByPk({
+    const productId = await Product.findByPk(req.params.id, {
       // be sure to include its associated Category and Tag data
       include: [
         {
@@ -56,7 +56,8 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
+
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -65,15 +66,6 @@ router.post('/', async (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-
-  router.post('/', async (req, res) => {
-    try {
-      const locationData = await Location.create(req.body);
-      res.status(200).json(locationData);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
 
   Product.create(req.body)
     .then((product) => {
@@ -95,7 +87,8 @@ router.post('/', async (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
-});
+  });
+
 
 // update product
 router.put('/:id', (req, res) => {
@@ -139,8 +132,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
+    try {
+        const destroyProduct = await Product.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!deleteProduct) {
+            res.status(404).json({ message: "No product found with that id!" });
+            return;
+        }
+
+        res.status(200).json(destroyProduct);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
